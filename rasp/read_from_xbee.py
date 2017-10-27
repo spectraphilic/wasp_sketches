72b6f44c4f5cdaae24e07a7517cb68f91881ce9d
meshpi <meshpi@raspberrypi>
2017-10-27 20:21:13 +0200

    Sending to server works

--- rasp/read_from_xbee.py
+++ rasp/read_from_xbee.py
@@ -1,24 +1,52 @@
+import base64
+import json
+import logging
+import sys
+from time import time
+
 from serial import Serial
 from xbee import XBee
 from paho.mqtt.client import Client
 
 
+logger = logging.getLogger('read_from_xbee')
+
 def on_connect(client, userdata, flags, rc):
-    print('Connected %s' % rc)
+    logger.info('Connected to MQTT server')
 
 if __name__ == '__main__':
+    # Configure logger
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    # Connect to MQTT server
     client = Client()
     client.on_connect = on_connect
     client.connect('localhost')
     client.loop_start()
 
+    # {'source_addr_long': '\x00\x13\xa2\x00Aj\x07#', 'rf_data': "<=>\x06\x1eb'g\x05|\x10T\x13#\xc3{\xa8\n\xf3Y4b\xc8\x00\x00PA33\xabA\x00\x00\x00\x00", 'source_addr': '\xff\xfe', 'id': 'rx', 'options': '\xc2'}
     with Serial('/dev/serial0', 9600) as serial:
         xbee = XBee(serial)
         while True:
             try:
                 frame = xbee.wait_read_frame()
-                print(frame)
-                client.publish('frames', frame['rf_data'])
+                t0 = time()
+                logger.debug('FRAME %s', frame)
+                if frame['id'] != 'rx':
+                    logger.warning('UNEXPECTED ID %s', frame['id'])
+                    continue
+
+                for k in frame.keys():
+                    if k != 'id':
+                        v = frame[k]
+                        frame[k] = base64.b64encode(v)
+                client.publish('frames', json.dumps(frame), qos=2)
+                logger.debug('FRAME sent in %f seconds', (time() - t0))
             except KeyboardInterrupt:
                 break
 
--- rasp/send_to_server.py
+++ rasp/send_to_server.py
@@ -1,16 +1,69 @@
-from paho.mqtt.client import Client
+import base64
+import json
+import logging
+import sqlite3
+import sys
 
+from paho.mqtt.client import Client
+import requests
+
+logger = logging.getLogger('send_to_server')
 
 def on_connect(client, userdata, flags, rc):
     print('Connected %s' % rc)
-    client.subscribe('frames')
+    client.subscribe('frames', qos=2)
 
 def on_message(client, userdata, msg):
-    print(msg.payload)
+    cursor = userdata
+    frame = json.loads(msg.payload)
+    print(frame)
+
+    # Archive to SQL
+    cursor.execute(
+        """INSERT INTO frames VALUES (?, ?, ?, ?, ?, ?)""",
+        (
+            0,
+            unicode(frame['id']),
+            sqlite3.Binary(frame['rf_data']),
+            sqlite3.Binary(frame['source_addr']),
+            sqlite3.Binary(frame['source_addr_long']),
+            sqlite3.Binary(frame['options']),
+        )
+    )
+    conn.commit()
+
+    # Send to server
+    headers = {'Authorization': 'Token b14609d9aa6cb0a129885f9a7088abcb700e16ee'}
+    requests.post(
+        'http://hycamp.org/wsn/api/create/', json=frame, headers=headers,
+    )
+
 
 if __name__ == '__main__':
-    client = Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect('localhost')
-    client.loop_forever()
+    # Configure logger
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    conn = sqlite3.connect('frames.db')
+    with conn:
+        cursor = conn.cursor()
+        # Create table
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS frames
+            (sent integer, id text, rf_data blob, source_addr blob, source_addr_long blob, options blob)
+            """)
+        conn.commit()
+
+        # Connect to MQTT server
+        client = Client(
+            client_id='send_to_server', clean_session=False,
+            userdata=cursor
+        )
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.connect('localhost')
+        client.loop_forever()
