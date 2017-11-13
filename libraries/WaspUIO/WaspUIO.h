80a2df97e4c69c270395fc09910a38f0d69fc9b1
J. David Ibáñez <jdavid.ibp@gmail.com>
2017-11-13 17:20:53 +0100

    Revert changes to USE_

--- libraries/WaspUIO/WaspUIO.h
+++ libraries/WaspUIO/WaspUIO.h
@@ -9,9 +9,9 @@
  ******************************************************************************/
 
 // We cannot enable everything at the same time or we run out of program memory
-#define USE_AGR true
-#define USE_I2C false // I include here OneWire as well
-#define USE_SDI false
+#define USE_AGR false
+#define USE_I2C true // I include here OneWire as well
+#define USE_SDI true
 #define FRAME_BINARY true
 
 #include <inttypes.h>
