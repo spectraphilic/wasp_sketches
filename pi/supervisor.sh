94854c40a4ed58c56bcb455688ae1241fdd5040a
J. David Ibáñez <jdavid.ibp@gmail.com>
2017-11-30 13:02:05 +0100

    pi: switch to Py3

--- pi/supervisor.sh
+++ pi/supervisor.sh
@@ -2,7 +2,7 @@ cat << EOF
 [program:read_from_xbee]
 directory=$PWD
 user=$USER
-command=$PWD/venv27/bin/python read_from_xbee.py
+command=$PWD/venv/bin/python read_from_xbee.py
 stderr_logfile=$PWD/log/read_from_xbee.err.log
 stdout_logfile=$PWD/log/read_from_xbee.out.log
 autostart=true
@@ -11,7 +11,7 @@ autorestart=true
 [program:archive]
 directory=$PWD
 user=$USER
-command=$PWD/venv27/bin/python archive.py
+command=$PWD/venv/bin/python archive.py
 stderr_logfile=$PWD/log/archive.err.log
 stdout_logfile=$PWD/log/archive.out.log
 autostart=true
@@ -20,7 +20,7 @@ autorestart=true
 ; [program:send_to_server]
 ; directory=$PWD
 ; user=$USER
-; command=$PWD/venv27/bin/python send_to_server.py
+; command=$PWD/venv/bin/python send_to_server.py
 ; stderr_logfile=$PWD/log/send_to_server.err.log
 ; stdout_logfile=$PWD/log/send_to_server.out.log
 ; autostart=true
