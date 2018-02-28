6c22fb4170d66c0634c878dfa3bc33e119f5579e
J. David Ibáñez <jdavid.ibp@gmail.com>
2018-02-28 12:30:10 +0100

    New command to read DS18B20 strings (read 7)

--- libraries/WaspUIO/program_sensors.cpp
+++ libraries/WaspUIO/program_sensors.cpp
@@ -372,21 +372,9 @@ CR_TASK(taskSdiDs2)
 
 CR_TASK(task1Wire)
 {
-  SdFile file;
-  int len;
-  char word[20];
-  uint8_t pin;
-  uint8_t addr[8];
-  uint8_t data[12];
-  char data_str[17];
-  uint8_t crc;
-  int16_t temp;
-  float temp_f;
-  char temp_str[20];
-  uint8_t max = 10;
+  uint8_t max = 40;
   int values[max];
-  uint8_t n = 0;
-  bool eol;
+  uint8_t n;
 
   // TODO
   // - spawn 1 task per pin
@@ -394,89 +382,9 @@ CR_TASK(task1Wire)
 
   CR_BEGIN;
 
-  if (SD.openFile("onewire.txt", &file, O_READ) == 0)
-  {
-    error(F("Missing onewire.txt file"));
-    CR_ERROR;
-  }
-
   UIO.on(UIO_1WIRE);
-  while ((len = file.fgets(word, sizeof(word), (char*)" \n")) > 0)
-  {
-    eol = (word[len-1] == '\n');
-    len--; word[len] = '\0'; // Remove delimiter
-    if (len == 1)
-    {
-      pin = (uint8_t) atoi(word);
-      WaspOneWire oneWire(_getPin(pin));
-      if (! oneWire.reset())
-      {
-        warn(F("OneWire no devices attached to %hhu pin"), pin);
-        continue;
-      }
-
-      // Send conversion command to all sensors
-      oneWire.skip();
-      oneWire.write(0x44, 1); // Keep power on (parasite mode)
-      delay(800);             // Time for the sensors to do their job
-
-      while ((len = file.fgets(word, sizeof(word), (char*)" \n")) > 0)
-      {
-        eol = (word[len-1] == '\n');
-        len--; word[len] = '\0'; // Remove delimiter
-        if (len == 16)
-        {
-          if (Utils.str2hex(word, addr, 8) < 8)
-          {
-            warn(F("OneWire(%hhu) bad address '%s'"), pin, word);
-            continue;
-          }
-
-          // Read value
-          oneWire.reset();
-          oneWire.select(addr);
-          oneWire.write(0xBE); // Read Scratchpad
-          oneWire.read_bytes(data, 9); // We need 9 bytes
-
-          crc = oneWire.crc8(data, 8);
-          if (crc == data[8])
-          {
-            temp = (data[1] << 8) | data[0];
-
-            // Debug
-            temp_f = (float) temp / 16;
-            Utils.float2String(temp_f, temp_str, 2);
-            debug(F("OneWire(%hhu) %s  : %s"), pin, word, temp_str);
-          }
-          else
-          {
-            temp = INT_MIN;
-            Utils.hex2str(data, data_str, 8);
-            warn(F("OneWire(%hhu) %s bad data, CRC failed: %s %02X"), pin, word, data_str, crc);
-          }
-          values[n] = temp;
-          n++;
-
-          if (n == max)
-          {
-            ADD_SENSOR(SENSOR_DS1820, n, values);
-            n = 0;
-          }
-        }
-
-	if (eol) { eol = false; break; } // EOL
-      }
-      oneWire.depower();
-    }
-  }
+  n = UIO.readDS18B20(values, max);
   UIO.off(UIO_1WIRE);
-
-  file.close();
-  if (len == -2)
-  {
-    error(F("Cannot read onewire.txt"));
-    CR_ERROR;
-  }
 
   if (n > 0)
   {
