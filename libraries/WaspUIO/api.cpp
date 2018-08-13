43de96decddecff623cdb02fe09dd56c42c4ee7c
J. David Ibáñez <jdavid.ibp@gmail.com>
2018-08-13 12:15:04 +0200

    Switch to api v033

--- libraries/WaspUIO/api.cpp
+++ libraries/WaspUIO/api.cpp
@@ -6,32 +6,6 @@
 
 
 /*
- * RTC uses I2C. If there are other sensors with I2C they must be powered on
- * when using RTC, otherwise it won't work.
- *
- * TODO Optimize, check configuration to switch on 3V3 only if there're I2C
- * devices. Consider I2C isolators as well.
- *
- */
-
-void TwoWire::secureBegin()
-{
-  // Do we need to switch on 5V as well?
-  _3V3_ON = UIO.v33(1);
-  if (! _3V3_ON)
-  {
-    delay(50);
-  }
-}
-
-
-void TwoWire::secureEnd()
-{
-  UIO.v33(_3V3_ON);
-}
-
-
-/*
  * The upstream code is written in such a way that it sends the mote name (id)
  * with every frame. But we only want to send it in uptime frame.
  *
