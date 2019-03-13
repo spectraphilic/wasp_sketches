8729d804d58c515d09ea0dc10cdff213259e97e6
J. David Ibáñez <jdavid.ibp@gmail.com>
2019-03-13 16:41:06 +0100

    4g test prints network type, operator and rssi
    Fixes #46

--- libraries/WaspUIO/commands_network.cpp
+++ libraries/WaspUIO/commands_network.cpp
@@ -2,6 +2,23 @@
 
 
 #if WITH_4G
+const char _4G_GPRS [] PROGMEM = "GPRS";
+const char _4G_EGPRS [] PROGMEM = "EGPRS";
+const char _4G_WCDMA [] PROGMEM = "WCDMA";
+const char _4G_HSDPA [] PROGMEM = "HSDPA";
+const char _4G_LTE [] PROGMEM = "LTE";
+const char _4G_UNKNOWN [] PROGMEM = "UNKNOWN";
+
+const char* const _4G_networks[] PROGMEM = {
+  _4G_GPRS,
+  _4G_EGPRS,
+  _4G_WCDMA,
+  _4G_HSDPA,
+  _4G_LTE,
+  _4G_UNKNOWN,
+};
+
+
 /**
  * Set APN (Access Point Name) for 4G module
  */
@@ -69,6 +86,42 @@ COMMAND(cmd4G_Test)
   if (err)
   {
     return cmd_error;
+  }
+
+  // Network type
+  err = _4G.getNetworkType();
+  if (err == 0 && _4G._networkType < sizeof(_4G_networks) / sizeof(const char*))
+  {
+    char name[10];
+    strcpy_P(name, (char*)pgm_read_word(&(_4G_networks[_4G._networkType])));
+    cr.println(F("Network: %s"), name);
+  }
+  else
+  {
+    cr.println(F("Network: ERROR"));
+  }
+
+  // Operator
+  char operator_name[20] = {0};
+  err = _4G.getOperator(operator_name);
+  if (err == 0)
+  {
+    cr.println(F("Operator: %s"), operator_name);
+  }
+  else
+  {
+    cr.println(F("Operator: ERROR"));
+  }
+
+  // RSSI
+  err = _4G.getRSSI();
+  if (err == 0)
+  {
+    cr.println(F("RSSI: %d dBm"), _4G._rssi);
+  }
+  else
+  {
+    cr.println(F("RSSI: ERROR"));
   }
 
   UIO._4GStop();
