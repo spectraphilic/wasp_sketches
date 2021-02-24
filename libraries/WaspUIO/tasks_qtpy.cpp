a8853ecf0a6e744d00be4595b8e86d13219a5933
J. David Ibáñez <jdavid.ibp@gmail.com>
2021-02-24 19:52:56 +0100

    QT Py: comment ICM block since it doesn't work

--- libraries/WaspUIO/tasks_qtpy.cpp
+++ libraries/WaspUIO/tasks_qtpy.cpp
@@ -19,7 +19,8 @@ CR_TASK(taskQTPY)
     if (n > 0) {
         if (ttt > 0) { CR_DELAY(ttt * 1000); }
         if (sdi.data(address) != NULL) {
-            uint16_t f1 = strtoul(sdi.buffer+1, &next, 10);
+            next = sdi.buffer + 1;
+            uint16_t f1 = strtoul(next, &next, 10);
             uint16_t f2 = strtoul(next, &next, 10);
             uint16_t f3 = strtoul(next, &next, 10);
             uint16_t f4 = strtoul(next, &next, 10);
@@ -37,18 +38,21 @@ CR_TASK(taskQTPY)
     if (n > 0) {
         if (ttt > 0) { CR_DELAY(ttt * 1000); }
         if (sdi.data(address) != NULL) {
-            float bme_t = strtod(sdi.buffer+1, &next);
+            next = sdi.buffer + 1;
+            float bme_t = strtod(next, &next);
             float bme_h = strtod(next, &next);
             float bme_p = strtod(next, &next);
             ADD_SENSOR(SENSOR_BME_77, bme_t, bme_h, bme_p);
         }
     }
 
+/*
     n = sdi.measure(&ttt, address, 2);
     if (n > 0) {
         if (ttt > 0) { CR_DELAY(ttt * 1000); }
         if (sdi.data(address) != NULL) {
-            float temp = strtod(sdi.buffer+1, &next);
+            next = sdi.buffer + 1;
+            float temp = strtod(next, &next);
             float acc_x = strtod(next, &next);
             float acc_y = strtod(next, &next);
             float acc_z = strtod(next, &next);
@@ -61,12 +65,14 @@ CR_TASK(taskQTPY)
             ADD_SENSOR(SENSOR_ICM20X, temp, acc_x, acc_y, acc_z, mag_x, mag_y, mag_z, gyro_x, gyro_y, gyro_z);
         }
     }
+*/
 
     n = sdi.measure(&ttt, address, 3);
     if (n > 0) {
         if (ttt > 0) { CR_DELAY(ttt * 1000); }
         if (sdi.data(address) != NULL) {
-            float mlx_o = strtod(sdi.buffer+1, &next);
+            next = sdi.buffer + 1;
+            float mlx_o = strtod(next, &next);
             float mlx_a = strtod(next, &next);
             ADD_SENSOR(SENSOR_MLX90614, mlx_o, mlx_a); // object, ambient
         }
@@ -76,7 +82,8 @@ CR_TASK(taskQTPY)
     if (n > 0) {
         if (ttt > 0) { CR_DELAY(ttt * 1000); }
         if (sdi.data(address) != NULL) {
-            float sht_t = strtod(sdi.buffer+1, &next);
+            next = sdi.buffer + 1;
+            float sht_t = strtod(next, &next);
             float sht_h = strtod(next, &next);
             ADD_SENSOR(SENSOR_SHT31, sht_t, sht_h);
         }
@@ -86,7 +93,8 @@ CR_TASK(taskQTPY)
     if (n > 0) {
         if (ttt > 0) { CR_DELAY(ttt * 1000); }
         if (sdi.data(address) != NULL) {
-            float temp = strtod(sdi.buffer+1, &next);
+            next = sdi.buffer + 1;
+            float temp = strtod(next, &next);
             ADD_SENSOR(SENSOR_TMP1XX, temp);
         }
     }
@@ -95,7 +103,8 @@ CR_TASK(taskQTPY)
     if (n > 0) {
         if (ttt > 0) { CR_DELAY(ttt * 1000); }
         if (sdi.data(address) != NULL) {
-            uint16_t prox = strtoul(sdi.buffer+1, &next, 10);
+            next = sdi.buffer + 1;
+            uint16_t prox = strtoul(next, &next, 10);
             uint16_t lux = strtoul(next, &next, 10);
             uint16_t white = strtoul(next, &next, 10);
             ADD_SENSOR(SENSOR_VCNL4040, prox, lux, white);
@@ -106,7 +115,8 @@ CR_TASK(taskQTPY)
     if (n > 0) {
         if (ttt > 0) { CR_DELAY(ttt * 1000); }
         if (sdi.data(address) != NULL) {
-            float lux = strtod(sdi.buffer+1, &next);
+            next = sdi.buffer + 1;
+            float lux = strtod(next, &next);
             float white = strtod(next, &next);
             uint16_t als = strtoul(next, &next, 10);
             ADD_SENSOR(SENSOR_VEML7700, lux, white, als);
