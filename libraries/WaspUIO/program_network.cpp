98919da2c576a89334820dba6ff04ac95b431744
J. David Ibáñez <jdavid.ibp@gmail.com>
2022-09-08 10:06:47 +0200

    Reformat libraries/WaspUIO/program_network.cpp

--- libraries/WaspUIO/program_network.cpp
+++ libraries/WaspUIO/program_network.cpp
@@ -4,62 +4,61 @@
 #if WITH_4G
 CR_TASK(taskNetwork4G)
 {
-  int size;
-  uint32_t t0;
-  uint8_t status;
-  uint8_t n;
+    int size;
+    uint32_t t0;
+    uint8_t status;
+    uint8_t n;
 
-  CR_BEGIN;
+    CR_BEGIN;
 
-  // Check we've at least 30s to send something
-  if (cr.timeout(UIO._epoch_millis, SEND_TIMEOUT - 30000L))
-  {
-    log_warn("No time left to send anything");
-    CR_RETURN;
-  }
-
-  if (UIO._4GStart())
-  {
-    log_error("_4GStart() failed");
-    CR_ERROR;
-  }
-
-  // Send frames
-  log_debug("Sending frames...");
-  while (!cr.timeout(UIO._epoch_millis, SEND_TIMEOUT))
-  {
-    t0 = millis();
-
-    // Read
-    size = UIO.readFrame(n);
-    if (size <= 0) { break; }
-
-    // Send
-    status = _4G.sendFrameToMeshlium((char*)"wsn.latice.eu", 80, (uint8_t*)SD.buffer, size);
-    if (status)
-    {
-      log_warn("_4G.sendFrameToMeshlium error=%d %d", status, _4G._errorCode);
-      break;
-    }
-    if (_4G._httpCode != 200) // Check HTTP status code of the response
-    {
-      log_error("Unexpect HTTP code %d", _4G._httpCode);
-      break;
+    // Check we've at least 30s to send something
+    if (cr.timeout(UIO._epoch_millis, SEND_TIMEOUT - 30000L)) {
+        log_warn("No time left to send anything");
+        CR_RETURN;
     }
 
-    // Next
-    log_debug("%d frame(s) sent in %lu ms", n, cr.millisDiff(t0));
-    UIO.ack_wait = n;
-    cmdAck(""); // Move to the next frame
+    if (UIO._4GStart()) {
+        log_error("_4GStart() failed");
+        CR_ERROR;
+    }
 
-    CR_DELAY(50); // Give control back
-  }
+    // Send frames
+    log_debug("Sending frames...");
+    while (!cr.timeout(UIO._epoch_millis, SEND_TIMEOUT)) {
+        t0 = millis();
 
-  // Set time from network
-  status = UIO.setTimeFrom4G();
-  UIO._4GStop(); // Switch off
+        // Read
+        size = UIO.readFrame(n);
+        if (size <= 0) {
+            break;
+        }
 
-  CR_END;
+        // Send
+        status = _4G.sendFrameToMeshlium((char*)"wsn.latice.eu", 80, (uint8_t*)SD.buffer, size);
+        if (status) {
+            log_warn("_4G.sendFrameToMeshlium error=%d %d", status, _4G._errorCode);
+            break;
+        }
+
+        // Check HTTP status code of the response
+        if (_4G._httpCode != 200) {
+            log_error("Unexpect HTTP code %d", _4G._httpCode);
+            break;
+        }
+
+        // Next
+        log_debug("%d frame(s) sent in %lu ms", n, cr.millisDiff(t0));
+        UIO.ack_wait = n;
+        cmdAck(""); // Move to the next frame
+
+        CR_DELAY(50); // Give control back
+    }
+
+    // Set time from network
+    status = UIO.setTimeFrom4G();
+    UIO._4GStop(); // Switch off
+
+    CR_END;
 }
 #endif
 
@@ -67,72 +66,68 @@ CR_TASK(taskNetwork4G)
 #if WITH_IRIDIUM
 CR_TASK(taskNetworkIridium)
 {
-  int size;
-  uint32_t t0;
-  int status;
-  int quality;
-  uint8_t n;
+    int size;
+    uint32_t t0;
+    int status;
+    int quality;
+    uint8_t n;
 
-  CR_BEGIN;
+    CR_BEGIN;
 
-  // Check we've at least 1m30 to send something
-  if (cr.timeout(UIO._epoch_millis, SEND_TIMEOUT - 90000L))
-  {
-    log_warn("No time left to send anything");
-    CR_RETURN;
-  }
-
-  status = UIO.iridium_start(); // This takes ~700ms
-  if (status != ISBD_SUCCESS)
-  {
-    CR_ERROR;
-  }
-
-  // Check signal quality, informational
-  status = iridium.getSignalQuality(quality); // This takes ~4s
-  if (status != ISBD_SUCCESS)
-  {
-    log_error("iridium.getSignalQuality(..) error=%d", status);
-    UIO.iridium_stop();
-    CR_ERROR;
-  }
-  log_debug("Iridium signal quality %d", quality);
-  if (quality == 0)
-  {
-    UIO.iridium_stop();
-    CR_ERROR;
-  }
-
-  // Send frames
-  log_debug("Sending frames...");
-  while (!cr.timeout(UIO._epoch_millis, SEND_TIMEOUT))
-  {
-    t0 = millis();
-
-    // Read
-    size = UIO.readFrame(n);
-    if (size <= 0) { break; }
-
-    // Send
-    status = iridium.sendSBDBinary((uint8_t*)SD.buffer, size);
-    if (status != ISBD_SUCCESS)
-    {
-      log_warn("iridium.sendSBDBinary(..) error=%d", status);
-      break;
+    // Check we've at least 1m30 to send something
+    if (cr.timeout(UIO._epoch_millis, SEND_TIMEOUT - 90000L)) {
+        log_warn("No time left to send anything");
+        CR_RETURN;
     }
 
-    // Next
-    log_debug("%d frame(s) sent in %lu ms", n, cr.millisDiff(t0));
-    UIO.ack_wait = n;
-    cmdAck(""); // Move to the next frame
+    status = UIO.iridium_start(); // This takes ~700ms
+    if (status != ISBD_SUCCESS) {
+        CR_ERROR;
+    }
 
-    CR_DELAY(50); // Give control back
-  }
+    // Check signal quality, informational
+    status = iridium.getSignalQuality(quality); // This takes ~4s
+    if (status != ISBD_SUCCESS) {
+        log_error("iridium.getSignalQuality(..) error=%d", status);
+        UIO.iridium_stop();
+        CR_ERROR;
+    }
+    log_debug("Iridium signal quality %d", quality);
+    if (quality == 0) {
+        UIO.iridium_stop();
+        CR_ERROR;
+    }
 
-  // Switch off
-  UIO.iridium_stop();
+    // Send frames
+    log_debug("Sending frames...");
+    while (!cr.timeout(UIO._epoch_millis, SEND_TIMEOUT)) {
+        t0 = millis();
 
-  CR_END;
+        // Read
+        size = UIO.readFrame(n);
+        if (size <= 0) {
+            break;
+        }
+
+        // Send
+        status = iridium.sendSBDBinary((uint8_t*)SD.buffer, size);
+        if (status != ISBD_SUCCESS) {
+            log_warn("iridium.sendSBDBinary(..) error=%d", status);
+            break;
+        }
+
+        // Next
+        log_debug("%d frame(s) sent in %lu ms", n, cr.millisDiff(t0));
+        UIO.ack_wait = n;
+        cmdAck(""); // Move to the next frame
+
+        CR_DELAY(50); // Give control back
+    }
+
+    // Switch off
+    UIO.iridium_stop();
+
+    CR_END;
 }
 #endif
 
@@ -150,15 +145,17 @@ CR_TASK(taskNetworkUSB)
         // Read
         if (UIO.ack_wait == 0) {
             int size = UIO.readFrame(n);
-            if (size <= 0)
+            if (size <= 0) {
                 break;
+            }
 
             // Dump the frame
             USB.println((uint8_t*)SD.buffer, size);
 
             // Next
             UIO.ack_wait = n;
-        } else if (cr.timeout(sent, 10 * 1000)) {
+        }
+        else if (cr.timeout(sent, 10 * 1000)) {
             break; // If waiting for an ACK more than 10s, stop sending.
         }
 
@@ -192,19 +189,22 @@ CR_TASK(taskNetworkXBee)
     CR_SPAWN(taskNetworkXBeeReceive);
 
     // Schedule sending frames
-    if (UIO.hasSD)
+    if (UIO.hasSD) {
         CR_SPAWN2(taskNetworkXBeeSend, tid);
+    }
 
     // Keep the network open, default is 45s
     wait = UIO.lan_wait ? (UIO.lan_wait * 1000UL) : 45000UL;
     CR_DELAY(wait);
 
-    if (UIO.hasSD)
+    if (UIO.hasSD) {
         CR_JOIN(tid);
+    }
 
-    // RSSI
-    if (UIO.xbeeQuality() == 0) // FIXME This outputs garbage to USB ??
+    // RSSI (FIXME This outputs garbage to USB?)
+    if (UIO.xbeeQuality() == 0) {
         ADD_SENSOR(SENSOR_RSSI, UIO.rssi);
+    }
 
     // Stop network
     if (xbeeDM.XBee_ON) {
@@ -217,74 +217,68 @@ CR_TASK(taskNetworkXBee)
 
 CR_TASK(taskNetworkXBeeSend)
 {
-  uint32_t t0;
-  static unsigned long sent;
-  uint8_t n;
+    uint32_t t0;
+    static unsigned long sent;
+    uint8_t n;
 
-  CR_BEGIN;
-  UIO.ack_wait = 0;
+    CR_BEGIN;
+    UIO.ack_wait = 0;
 
-  // Send frames
-  while (!cr.timeout(UIO._epoch_millis, SEND_TIMEOUT))
-  {
-    if (UIO.ack_wait == 0)
-    {
-      // Read
-      int size = UIO.readFrame(n);
-      if (size <= 0) { break; }
+    // Send frames
+    while (!cr.timeout(UIO._epoch_millis, SEND_TIMEOUT)) {
+        if (UIO.ack_wait == 0) {
+            // Read
+            int size = UIO.readFrame(n);
+            if (size <= 0) {
+                break;
+            }
 
-      // Send the frame
-      t0 = millis();
-      if (xbeeDM.send((char*)UIO.xbee.rx_address, (uint8_t*)SD.buffer, size) == 1)
-      {
-        log_warn("xbeeDM.send(..) failure");
-        break;
-      }
-      sent = millis();
+            // Send the frame
+            t0 = millis();
+            if (xbeeDM.send((char*)UIO.xbee.rx_address, (uint8_t*)SD.buffer, size) == 1) {
+                log_warn("xbeeDM.send(..) failure");
+                break;
+            }
+            sent = millis();
 
-      // Next
-      log_debug("%d frame(s) sent in %lu ms", n, cr.millisDiff(t0));
-      UIO.ack_wait = n;
+            // Next
+            log_debug("%d frame(s) sent in %lu ms", n, cr.millisDiff(t0));
+            UIO.ack_wait = n;
+        }
+        else if (cr.timeout(sent, 10 * 1000)) {
+            break; // If waiting for an ACK more than 10s, stop sending.
+        }
+
+        CR_DELAY(50); // Give control back
     }
-    else if (cr.timeout(sent, 10 * 1000))
-    {
-      break; // If waiting for an ACK more than 10s, stop sending.
-    }
 
-    CR_DELAY(50); // Give control back
-  }
-
-  CR_END;
+    CR_END;
 }
 
 CR_TASK(taskNetworkXBeeReceive)
 {
-  char sourceMAC[17];
+    char sourceMAC[17];
 
-  CR_BEGIN;
+    CR_BEGIN;
 
-  while (xbeeDM.XBee_ON)
-  {
-    if (xbeeDM.available())
-    {
-      // Data is expected to be available before calling this method, that's
-      // why we only timeout for 100ms, less should be enough
-      if (xbeeDM.receivePacketTimeout(100))
-      {
-        log_warn("receivePacket: timeout (we will retry)");
-      }
-      else
-      {
-        Utils.hex2str(xbeeDM._srcMAC, sourceMAC, 8);
-        exeCommands((const char*)xbeeDM._payload, false);
-      }
+    while (xbeeDM.XBee_ON) {
+        if (xbeeDM.available()) {
+            // Data is expected to be available before calling this method, that's
+            // why we only timeout for 100ms, less should be enough
+            if (xbeeDM.receivePacketTimeout(100)) {
+                log_warn("receivePacket: timeout (we will retry)");
+            }
+            else {
+                Utils.hex2str(xbeeDM._srcMAC, sourceMAC, 8);
+                exeCommands((const char*)xbeeDM._payload, false);
+            }
+        }
+
+        // Give control back
+        CR_DELAY(0);
     }
 
-    // Give control back
-    CR_DELAY(0);
-  }
-
-  CR_END;
+    CR_END;
 }
 #endif
 
@@ -296,8 +290,9 @@ CR_TASK(taskNetworkLora)
 
     CR_BEGIN;
 
-    if (! UIO.hasSD)
+    if (! UIO.hasSD) {
         CR_ERROR;
+    }
 
     if (UIO.loraStart()) {
         log_error("taskNetworkLora loraStart() failed");
@@ -306,8 +301,9 @@ CR_TASK(taskNetworkLora)
     log_info("LoRa started");
 
     // Schedule sending frames. Don't send with Lora if I'm the gateway.
-    if (UIO.wan_type == WAN_DISABLED)
+    if (UIO.wan_type == WAN_DISABLED) {
         CR_SPAWN2(taskNetworkLoraSend, tid);
+    }
 
     // Receive: Senders need to listen as well for cmdAck
     CR_SPAWN(taskNetworkLoraReceive);
@@ -316,8 +312,9 @@ CR_TASK(taskNetworkLora)
     wait = UIO.lan_wait ? (UIO.lan_wait * 1000UL) : 45000UL;
     CR_DELAY(wait);
 
-    if (UIO.wan_type == WAN_DISABLED)
+    if (UIO.wan_type == WAN_DISABLED) {
         CR_JOIN(tid);
+    }
 
     // RSSI
     if (UIO.loraQuality() == 0) {
@@ -360,7 +357,8 @@ CR_TASK(taskNetworkLoraSend)
     if (dst == 0) {
         if (UIO.loraSend(UIO.lora_dst, "ping", true)) {
             CR_ERROR;
-        } else {
+        }
+        else {
             dst = UIO.lora_dst2;
             log_info("Lora auto mode dst=%hhu", dst);
         }
@@ -371,8 +369,9 @@ CR_TASK(taskNetworkLoraSend)
         if (UIO.ack_wait == 0) {
             // Read
             int size = UIO.readFrame(n_frames);
-            if (size <= 0)
+            if (size <= 0) {
                 break;
+            }
 
             // Send the frame. The timeout is calculated by sx1272
             t0 = millis();
@@ -382,7 +381,7 @@ CR_TASK(taskNetworkLoraSend)
             }
             n_packages++;
             log_info("Sent packet=%hhu with %hhu frame(s) to dst=%hhu in %lu ms",
-                sx1272.packet_sent.packnum, n_frames, dst, cr.millisDiff(t0));
+                     sx1272.packet_sent.packnum, n_frames, dst, cr.millisDiff(t0));
 
             // Debug
 //          char str[50];
@@ -393,15 +392,17 @@ CR_TASK(taskNetworkLoraSend)
 
             // Next
             UIO.ack_wait = n_frames;
-        } else if (cr.timeout(t0, 10 * 1000)) {
+        }
+        else if (cr.timeout(t0, 10 * 1000)) {
             // Max 10s waiting for an ACK
             // If didn't receive the ACK for the 1st frame consider it a failure
             if (n_packages == 1) {
                 UIO.lora_fails++;
                 log_warn("Didn't get an ACK in this loop, fails=%hhu", UIO.lora_fails);
                 // If too many fails reset dst2 (this only has an effect in auto routing mode)
-                if (UIO.lora_fails >= LORA_MAX_FAILS)
+                if (UIO.lora_fails >= LORA_MAX_FAILS) {
                     UIO.lora_dst2 = 0;
+                }
             }
             break;
         }
@@ -424,17 +425,21 @@ CR_TASK(taskNetworkLoraReceive)
             if (strncmp("ping", data, 4) == 0) {
                 log_info("ping received from lora address=%u", sx1272.packet_received.src);
                 if (sx1272.packet_received.dst == UIO.lora_addr || sx1272.packet_received.src > UIO.lora_addr) {
-                    if (sx1272.setACK() || sx1272.sendWithTimeout())
+                    if (sx1272.setACK() || sx1272.sendWithTimeout()) {
                         log_warn("Lora: Failed to send ACK");
-                } else {
+                    }
+                }
+                else {
                     log_info("ignore ping");
                 }
-            } else if (strncmp("<=>", data, 3) == 0) {
+            }
+            else if (strncmp("<=>", data, 3) == 0) {
                 log_debug("frame received from lora address=%u", sx1272.packet_received.src);
+                // dst(1) + src(1) + packnum(1) + length(1) + data + retry(1)
                 uint8_t n = UIO.saveFrames(
                     sx1272.packet_received.src,
                     sx1272.packet_received.data,
-                    sx1272.packet_received.length - 5 // dst(1) + src(1) + packnum(1) + length(1) + data + retry(1)
+                    sx1272.packet_received.length - 5
                 );
                 if (n > 0) {
                     const int size = 12;
@@ -442,7 +447,8 @@ CR_TASK(taskNetworkLoraReceive)
                     cr_snprintf(cmd, size, "ack %u", n);
                     UIO.loraSend(sx1272.packet_received.src, cmd, false);
                 }
-            } else {
+            }
+            else {
                 log_debug("command received from lora address=%u", sx1272.packet_received.src);
                 exeCommands(data, false);
             }
